5e04f40069da178854ac6cae0afe2494db15b14c
wpsilva <wpsliva@maisaedu.com.br>
2022-10-04 13:53:06 -0300

    Adjust list routes

--- lib/app/module/login/login_module.dart
+++ lib/app/module/login/login_module.dart
@@ -5,11 +5,11 @@ import '_export.dart';
 class LoginModule extends Module {
   @override
   final List<Bind> binds = [
-    Bind.lazySingleton((i) => LoginStore()),
+    Bind.lazySingleton((i) => LoginStore())
   ];
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute('/', child: (_, args) => const LoginPage()),
+    ChildRoute('/', child: (_, args) => const LoginPage())
   ];
 }
